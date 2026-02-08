8c37f9fd020feda753febca38d02b1265f640a9c
Rodney Amos Chery <rodneyamoschery@Rodneys-Air.lan>
2026-02-08 04:33:10 -0500

    deployed web app to azure

--- obj/Debug/net10.0/RodneyPortfolio.AssemblyInfo.cs
+++ obj/Debug/net10.0/RodneyPortfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RodneyPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+532550c9e46c0a306f8af3c1336f4f05f52c82ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2991eb18bd6ecbfdead1d8e012a8288846795e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("RodneyPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RodneyPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
