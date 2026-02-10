c3075a29ff4a2a978306608b3b26e3a77eca009d
Rodney Amos Chery <rodneyamoschery@Rodneys-Air.lan>
2026-02-10 12:00:19 -0500

    refactor

--- obj/Debug/net10.0/RodneyPortfolio.AssemblyInfo.cs
+++ obj/Debug/net10.0/RodneyPortfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RodneyPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28f7e1f6a59c1863d4b8a80316133792d0d8e429")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b0f19e4db2dbcb9936e1bcc956af21090b88204")]
 [assembly: System.Reflection.AssemblyProductAttribute("RodneyPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RodneyPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
